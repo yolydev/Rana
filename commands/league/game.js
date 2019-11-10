d6db81cbedb95df3a1fb80b1bd1e36744d01077b
yolydev <philip.facebook@gmx.at>
2019-11-10 15:32:39 +0100

    embed title, description and mastery tab / mastery emojis

--- commands/league/game.js
+++ commands/league/game.js
@@ -52,41 +52,47 @@ module.exports = class GameCommand extends Command {
         try {
             const summoner = await api.get('euw1', 'summoner.getBySummonerName', getLeagueName);
             const spectator = await api.get('euw1', 'spectator.getCurrentGameInfoBySummoner', summoner.id);
-
+            console.log(spectator.observers);
             if(spectator == null) {
                 message.reply(`**${getLeagueName}** currently not in-game`); return;
             }
 
-            var teamBlueSummoners = "", teamBlueLeagues = "";
-            var teamRedSummoners = "", teamRedLeagues = "";
+            var teamBlueSummoners = "", teamBlueLeagues = "", teamBlueChampionPoints = "";
+            var teamRedSummoners = "", teamRedLeagues = "", teamRedChampionPoints = "";
 
             for(var i = 0; i < 10; i++) {
+                const mastery = await api.get('euw1', 'championMastery.getChampionMastery', spectator.participants[i].summonerId, spectator.participants[i].championId);
                 const league = await api.get('euw1', 'league.getLeagueEntriesForSummoner', spectator.participants[i].summonerId);
                 let entry = league.find(e => e.queueType === 'RANKED_SOLO_5x5');
-                
                 
                 if(spectator.participants[i].teamId === 100) {
                     teamBlueSummoners += `<:${translate.Champions[spectator.participants[i].championId]}:${emoji[translate.Champions[spectator.participants[i].championId]]}> | ${spectator.participants[i].summonerName}\n`;
                     
-                    if(entry === undefined) teamBlueLeagues += `Unranked\n`;
+                    if(entry === undefined) teamBlueLeagues += `Unranked${fillerEmoji}\n`;
                     else teamBlueLeagues += `<:${entry.tier}:${emoji[entry.tier]}>${(entry.tier).charAt(0).toUpperCase() + (entry.tier).slice(1).toLowerCase()} ${entry.rank} (${entry.leaguePoints} LP)\n`;
+
+                    if(mastery === null) teamBlueChampionPoints += `${fillerEmoji}0\n`;
+                    else teamBlueChampionPoints += `<:${mastery.championLevel}:${emoji[mastery.championLevel]}> ${mastery.championPoints}\n`;
                 } else if(spectator.participants[i].teamId === 200) {
                     teamRedSummoners += `<:${translate.Champions[spectator.participants[i].championId]}:${emoji[translate.Champions[spectator.participants[i].championId]]}> | ${spectator.participants[i].summonerName}\n`;
 
-                    if(entry === undefined) teamRedLeagues += `Unranked\n`;
+                    if(entry === undefined) teamRedLeagues += `Unranked${fillerEmoji}\n`;
                     else teamRedLeagues += `<:${entry.tier}:${emoji[entry.tier]}>${(entry.tier).charAt(0).toUpperCase() + (entry.tier).slice(1).toLowerCase()} ${entry.rank} (${entry.leaguePoints} LP)\n`;
+
+                    if(mastery === null) teamRedChampionPoints += `${fillerEmoji}0\n`;
+                    else teamRedChampionPoints += `<:${mastery.championLevel}:${emoji[mastery.championLevel]}> ${mastery.championPoints}\n`;
                 }
             }           
 
             const embed = new RichEmbed()
-                .setTitle('Test')
-                .setDescription('Test')
+                .setTitle(`${text}'s current game`)
+                .setDescription(`${translate.Queues[spectator.gameQueueConfigId]}`)
                 .addField('Blue Team', `${teamBlueSummoners}`, true)
                 .addField('Rank', `${teamBlueLeagues}`, true)
-                .addField('Mastery', 'Soon', true)
+                .addField('Mastery', `${teamBlueChampionPoints}`, true)
                 .addField('Red Team', `${teamRedSummoners}`, true)
                 .addField('Rank', `${teamRedLeagues}`, true)
-                .addField('Mastery', 'Soon', true)
+                .addField('Mastery', `${teamRedChampionPoints}`, true)
             message.channel.send(embed);
         } catch(e) {
             console.error(e);
